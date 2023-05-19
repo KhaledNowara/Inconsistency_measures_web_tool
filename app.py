47486ac4640dbbab0693e306e0ca751c294bbc27
KhaledNowara <kn_nowara@outlook.com>
2023-05-19 21:23:19 +0300

    mvp front end page, added post request

--- app.py
+++ app.py
@@ -1,10 +1,23 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, jsonify
 
 app = Flask(__name__)
 
 @app.route('/')
 def index ():
     return render_template('chat.html')
+
+@app.route('/process', methods=['POST'])
+def process_data():
+    data = request.get_json()
+    input_text = data.get('input_text')
+    selected_options = data.get('selected_options')
+
+    print(input_text)
+    # Do something with the received data
+    # ...
+
+    response = {'message': 'Data processed successfully'}
+    return jsonify(response)
 
 if __name__ == "__main__":
     app.run(debug=True)
--- static/js/chat.js
+++ static/js/chat.js
@@ -8,7 +8,7 @@ hamburgerMenu.addEventListener('click', function() {
   menu.classList.toggle('open');
   
     document.documentElement.style.setProperty('--content-margin', '300px'); // Adjust the margin as needed
-    console.log("I am trying")
+
 
 });
 
@@ -20,6 +20,41 @@ document.addEventListener('click', function(event) {
   if (!menu.contains(targetElement) && !hamburgerMenu.contains(targetElement)) {
     hamburgerMenu.classList.remove('open');
     menu.classList.remove('open');
-    document.documentElement.style.setProperty('--content-margin', '0px'); // Reset the margin
+    document.documentElement.style.setProperty('--content-margin', '20px'); // Reset the margin
   }
 });
+function resetTextBox() {
+  document.getElementById('textbox').value = "";
+}
+
+
+function sendData() {
+  const inputText = document.getElementById('textbox').value;
+  const checkboxes = document.querySelectorAll('input[type="checkbox"]:checked');
+  const selectedOptions = Array.from(checkboxes).map(checkbox => checkbox.value);
+
+  const data = {
+    input_text: inputText,
+    selected_options: selectedOptions
+  };
+
+  console.log(data)
+  fetch('/process', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify(data)
+  })
+  .then(response => response.json())
+  .then(data => {
+    console.log(data.message);
+    // Do something with the response data
+  })
+  .catch(error => {
+    console.error('Error:', error);
+  });
+
+  document.getElementById('textbox').value = "";
+
+}
